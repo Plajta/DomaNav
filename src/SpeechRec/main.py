84c0d585fe00d468718174ef33f9c44ff6b66160
LosVocelos <janocelik@gmail.com>
2023-03-03 23:17:55 +0100

    MERGE PIČO!

--- src/SpeechRec/main.py
+++ src/SpeechRec/main.py
@@ -2,23 +2,23 @@ from requests.auth import HTTPDigestAuth
 import requests
 from pydub import AudioSegment
 from pydub.playback import play
-import whisper
 
 ROOT = "https://services.speechtech.cz/tts/v4"
 USERNAME = "aimtechackathon"
 PASSWORD = "lu7Eabuu7E"
 
 def SpeechToText():
-    model = whisper.load_model("small")
-    audio = whisper.load_audio("src/SpeechRec/out/output.mp3")
-    audio = whisper.pad_or_trim(audio)
+    params = {
+        'format': 'plaintext',
+    }
 
-    # make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-    options = whisper.DecodingOptions(language = "cs", fp16 = False)
+    with open('src/SpeechRec/out/output.mp3', 'rb') as f:
+        data = f.read()
 
-    result = whisper.decode(model, mel, options)
-    print(result.text)
+    response = requests.post('https://uwebasr.zcu.cz/api/v1/CLARIN_ASR/CZ', params=params, data=data)
+    response.raise_for_status()
+    
+    print(response.text)
 
 def TextToSpeech(text):
     text = text.replace(" ", ".")
@@ -32,10 +32,10 @@ def TextToSpeech(text):
                  )
     r.raise_for_status()
 
-    with open("src/SpeechRec/out/output.mp3", "wb") as fw:
+    with open("output.mp3", "wb") as fw:
         fw.write(r.content)
 
-    audio = AudioSegment.from_mp3("src/SpeechRec/out/output.mp3")
+    audio = AudioSegment.from_mp3("output.mp3")
     play(audio)
 
 TextToSpeech()
