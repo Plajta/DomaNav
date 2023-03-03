1a9f3fad95768919fd9fc9a3ddc7db871aef803b
HelloWorld7894 <danpojhan@protonmail.com>
2023-03-03 21:53:44 +0100

    Text to speech

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-#blank
+from requests.auth import HTTPDigestAuth
+import requests
+
+ROOT = "https://services.speechtech.cz/tts/v4"
+USERNAME = "aimtechackathon"
+PASSWORD = "lu7Eabuu7E"
+
+def SpeechToText():
+    pass
+
+def TextToSpeech():
+    r = requests.get(ROOT+"/synth",
+                 data={"engine": "Iva30",  # Iva30, Jan30
+                       "text": "Vítejte a syntetizujte",
+                       "format": "mp3",
+                       },
+                  auth=HTTPDigestAuth(USERNAME, PASSWORD),
+                 )
+    r.raise_for_status()
+
+    with open("output.mp3", "wb") as fw:
+        fw.write(r.content)
