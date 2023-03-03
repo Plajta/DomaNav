49c3826c5276c6b8b96599b8deaf3db8d6476648
HelloWorld7894 <danpojhan@protonmail.com>
2023-03-03 22:45:35 +0100

    Added openai whisper

--- src/SpeechRec/main.py
+++ src/SpeechRec/main.py
@@ -2,18 +2,30 @@ from requests.auth import HTTPDigestAuth
 import requests
 from pydub import AudioSegment
 from pydub.playback import play
+import whisper
 
 ROOT = "https://services.speechtech.cz/tts/v4"
 USERNAME = "aimtechackathon"
 PASSWORD = "lu7Eabuu7E"
 
 def SpeechToText():
-    pass
+    model = whisper.load_model("small")
+    audio = whisper.load_audio("src/SpeechRec/out/output.mp3")
+    audio = whisper.pad_or_trim(audio)
 
-def TextToSpeech():
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    options = whisper.DecodingOptions(language = "cs", fp16 = False)
+
+    result = whisper.decode(model, mel, options)
+    print(result.text)
+
+def TextToSpeech(text):
+    text = text.replace(" ", ".")
+
     r = requests.get(ROOT+"/synth",
                  data={"engine": "Iva30",  # Iva30, Jan30
-                       "text": "Vítejte a syntetizujte",
+                       "text": text,
                        "format": "mp3",
                        },
                   auth=HTTPDigestAuth(USERNAME, PASSWORD),
@@ -27,3 +39,4 @@ def TextToSpeech():
     play(audio)
 
 TextToSpeech()
+SpeechToText()
