e3facc0d9902a5f264fb258bad6e9f8ffbcb062c
LosVocelos <janocelik@gmail.com>
2023-03-04 14:05:25 +0100

    WTF IS THIS?!

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from SpeechRec import speech_funcs
 #
 
 #TODO:
-#1) akcelerometr, gyroskop, atd.
-#2) Kam jdeš?
-#3) lokalizace
-#4) graf
-#5) cesta
-#6) kontrola člověka
+#1) akcelerometr, gyroskop, atd. - andri na tom pracuje
+#2) Kam jdeš? - done
+#3) lokalizace - dominik + vašek
+#4) graf - jéňa + karel + zapomněl jsem jméno
+#5) cesta - i dunno
+#6) kontrola člověka - done
 #7) konec?
