14dae4686ad545061a184ca4a2f0f7e6237c072c
HelloWorld7894 <danpojhan@protonmail.com>
2023-03-04 08:15:24 +0100

    Added some quite dumb todo list, added main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from SpeechRec import speech_funcs
+
+#
+# actual code for controlling a human
+#
+
+#TODO:
+#1) akcelerometr, gyroskop, atd.
+#2) Kam jdeš?
+#3) lokalizace
+#4) graf
+#5) cesta
+#6) kontrola člověka
+#7) konec?
