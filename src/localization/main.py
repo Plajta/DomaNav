9b7d1feb6b165cf63a743673b186380391c8b212
Vesek <venda.straka@gmail.com>
2023-03-04 12:42:45 +0100

    Finally working scanner, now with 100% more ESP

--- src/localization/main.py
+++ src/localization/main.py
@@ -1,14 +1,40 @@
-import os
-if os.name == "nt":
-    from windows import scanner
-elif os.name == "posix":
-    from unix import scanner
-
+import serial
 
 class localizator:
     def __init__ (self):
-        self.Scanner = scanner()
+        self.port = ""
+        with open('port.conf', encoding="utf-8") as f:
+            self.port = f.read()
+            print(self.port)
+        ser = serial.Serial(self.port, 115200, timeout=3)
+        ser.write(b'r')
+        ser.close()
+
+    def scan (self,cmd='u'):
+        scan_output = ""
+        ser = serial.Serial(self.port, 115200, timeout=3)
+        ser.write(cmd.encode("ASCII"))
+        while True:
+            znak = ser.read()
+            if znak != b'\x00':
+                scan_output = scan_output + str(znak.decode("ASCII"))
+            else:
+                print(scan_output.count('\n'))
+                ser.close()
+                break
+        ser.close()
+        return scan_output
+
 
 
 if __name__ == "__main__":
+    mapa = "";
     locate = localizator()
+    for i in range(2):
+        input("Stiskni enter pro načtení " + str(i) + " mapy")
+        aktualni = locate.scan('m')
+        print (aktualni)
+        mapa = mapa + aktualni
+    #print(locate.scan())
+    with open('wifi.map', 'w', encoding="utf-8") as f:
+        f.write(mapa)
