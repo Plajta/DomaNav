d24f4acd76746d5115ce983c3c7c20050148bc55
Smrk24 <82822206+Smrk24@users.noreply.github.com>
2023-03-04 02:30:27 +0100

    formátování kódu

--- src/navigace/node.py
+++ src/navigace/node.py
@@ -1,11 +1,11 @@
 class node:
-    x=0.0 
-    y=0.0
+    
     neighbours=()
     used=False
     name=""
-    def __init__(self,n,name) -> None:
+    def __init__(self, n, x, y) -> None:
 
-        self.neighbours=n
-
+        self.neighbours = n
+        self.x = x
+        self.y = y
         pass
