bd9c9954d190da60426e08d05ad1b50d1793829d
HelloWorld7894 <danpojhan@protonmail.com>
2023-03-04 19:20:51 +0100

    POGGERS Gitstatus gitstatus gitstatus

--- src/localization/finder.py
+++ src/localization/finder.py
@@ -8,15 +8,16 @@ class finder:
         with open('port.conf', encoding="utf-8") as f:
             self.port = f.read()
         self.map = self.map.split('\n')
-        self.decoded_map = list()
-        oblast_list = list()
+        self.decoded_map = []
+        oblast_list = []
         for line in self.map:
             line = line.split(';')
             if line[0] != "oblast":
                 oblast_list.append(line)
             else:
-                self.decoded_map.append(oblast_list)
-                oblast_list.clear()
+                if line[1] != str(1):
+                    self.decoded_map.append(oblast_list.copy())
+                    oblast_list.clear() #this is fricking disgusting
 
 
     def find(self):
@@ -24,15 +25,38 @@ class finder:
         scan = loc.scan()
         out = [step.split(';') for step in scan.split('\n')]
         zone_counter = 0
-        score = [0] * len(self.decoded_map)
+        score = [0]*len(self.decoded_map)
+        score_current_zone = 0
+        score_zones = []
+        """
+        score12 = [0] * len(self.decoded_map)
         for i,zone in enumerate(self.decoded_map):
+            print(zone)
             for bssid_in_zone in zone:
                 for bssid in out:
                     if(bssid[0]==bssid_in_zone[0]):
-                        print(str(bssid[0]) + ", Zóna: " + str(i))
-                        score[i]+=1
+                        print(str(bssid[0])+ ", Síla: " + str(bssid_in_zone[1]) + ", Zóna: " + str(i))
+                        # score[i]-=abs(int(bssid[1])-int(bssid_in_zone[1]))
+                    score[i]+=int(bssid[1])
+                    score12[i]+=int(bssid_in_zone[1])
         print(score)
+        """
+        for i, zone in enumerate(self.decoded_map):
+            zone_sum = 0
+            for bssid_in_zone in zone:
+                zone_sum += int(bssid_in_zone[1])
+
+            score_zones.append(round(zone_sum / len(zone), 2))
         
+        zone_sum = 0
+        for bssid in out:
+            zone_sum += int(bssid[1])
+        score_current_zone = round(zone_sum / len(out), 2)
+
+        print(score_zones)
+        print(score_current_zone)
+
+
 if __name__ == "__main__": # When run as a standalone module it tries to find you
     Finder = finder()
     Finder.find()
