967144e8bfb67d22241f390424939cf1eea4e420
Vesek <venda.straka@gmail.com>
2023-03-05 09:39:55 +0100

    QUICK TEST GUI

---
+++ src/gui.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+from localization.finder import finder
+
+f = finder()
+index, len = f.find()
+
+img = np.zeros((400,500))
+x = index%4
+y = index//5
+x *= 100
+y *= 100
+print("X: "+str(x)+" Y: "+str(y))
+img[x:x+100,y:y+100]=1
+
+cv2.imshow('L',img)
+cv2.waitKey(0)
